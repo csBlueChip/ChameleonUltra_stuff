72e5aec1fa6c1a32689f32a168dc8e43db6ef56b
BlueChip <csbluechip@gmail.com>
2026-06-24 20:43:55 +0100

    cleanup readAmiibo script

--- bash_lib/demo.sh
+++ bash_lib/demo.sh
@@ -4,33 +4,26 @@
 
 ESC=$'\e'
 cBWHT="${ESC}[1;37m"
-cBRED="${ESC}[1;31m"
-cBGRN="${ESC}[1;32m"
 cNORM="${ESC}[0m"
 
 # ----- edit to taste, or add a cli parser
 #cuCli="ChameleonUltra/software/script/chameleon_cli_main.py"
-[[ -h cu-cli ]] && cuCli=./cu-cli || cuCli="$1"
-
+cuCli="$1"
 pc2cu="/tmp/cu_cu2pc"
 cu2pc="/tmp/cu_pc2cu"
-
 cutty="/dev/ttyACM0"
-
 
 # ----- i like brevity
 declare -n  CAP=CU_CAP
 
 # ----- setup
 Setup  "$pc2cu"  "$cu2pc"  "$cuCli"  ||  exit $?
-Listen -q                      # wait for FIRST prompt
-#Show                           # show output
-echo "# Chameleon Ultra Daemon Started"
+Listen                         # wait for FIRST prompt
+Show                           # show output
 
 # ----- connect
 n=0
 q=""
-echo -n "# Connect : "
 while (( n == 0 )) ; do
 	Send $q "hw connect -p ${cutty}"  # send connect command
 	Listen -c -q
@@ -40,9 +33,13 @@ done
 Status n desc
 echo "${cBWHT}# Status: $n:|$desc|${cNORM}"
 
+# ----- do a thing
+Send "help"     || exit $?     # send "help" (shown next to prompt)
+Listen -q -c    || exit $?     # -quiet (noecho) -capture (to $CAP[])
+Show                           # show captured output
+
 # ----- do some stuff surrounded by logic
 q=""
-echo -n "* Present Amiibo: > "
 while true ; do
 	Send $q "hf mfu rdpg -p 2"
 	Listen -c -q  || exit $?     # -capture (to $CAP[])
@@ -53,6 +50,7 @@ done
 
 echo -en "${cBWHT}# Page 2: $data"
 cnt=0 ; [[ ${data:2:2} == "48" ]] && cnt=135 || cnt=238
+((cnt)) || exit 135
 echo " - Read $cnt pages${cNORM}"
 
 #=======================================================================================================================
@@ -72,25 +70,25 @@ echo " - Read $cnt pages${cNORM}"
 # Read all the pages
 #
 tag=""
-echo -n "${cBRED}|${cNORM}"
-for ((i = 0;  i < cnt;  i++)) ; do          # loop
-	Send -q "hf mfu rdpg -p $i"             #   send read-page
-	Listen -q -c                            #   get reply
-	[[ ${CAP[0]} == *"Data"* ]] && {        #   we have "Data"
-		data=${CAP[0]##* }                  #     extract the hex-data
-		[[ -z $data ]] && {                 #     did we get <blank>
-			(( i==234 || i==235 )) && {     #       we expect nothing on these 2 pages
-				data="5caff01d"             #         inject some scaffolding
-			} || {                          #       we expect data on all other pages
-				i=$((--i))                  #         decrement page counter
-				continue                    #         and go to the next page (ie. this page)
-			}                               #       .
-		}                                   #     .
-		tag="${tag}${data}"                 #     add the data to the tag string
-		echo -n "${data}${cBGRN}|${cNORM}"  #     friendly output for user
-	}                                       #   .
-done                                        # end.loop
-echo -e "\b${cBRED}|${cNORM}"               # friendly output for user
+echo -n "|["
+for ((i = 0;  i < cnt;  i++)) ; do       # loop
+	Send -q "hf mfu rdpg -p $i"          #   send read-page
+	Listen -q -c                         #   get reply
+	[[ ${CAP[0]} == *"Data"* ]] && {     #   we have "Data"
+		data=${CAP[0]##* }               #     extract the hex-data
+		[[ -z $data ]] && {              #     did we get <blank>
+			(( i==235 || i==236 )) && {  #       we expect nothing on these 2 pages
+				data="5caff01d"          #         inject some scaffolding
+			} || {                       #       we expect data on all other pages
+				i=$((--i))               #         decrement page counter
+				continue                 #         and go to the next page (ie. this page)
+			}                            #       .
+		}                                #     .
+		tag="${tag}${data}"              #     add the data to the tag string
+		echo -n "${data}|"               #     friendly output for user
+	}                                    #   .
+done                                     # end.loop
+echo -e "\b]|"
 
 #==============================================================================
 #==============================================================================
@@ -150,47 +148,37 @@ Uid2Pwd() {
 INTSAK=`Peek  2,1:1`
 [[ $INTSAK == "48" ]] && {
 	tuid=`Peek  0,0:3  1,0:4`
-	echo "# UID = ${tuid:0:6}'${tuid:6}"
 } || {
 	tuid=`Peek  0,0:4  1,0:3`
-	echo "# UID = ${tuid:0:8}'${tuid:8}"
 }
 
 # calculate the tag password
 Uid2Pwd "$tuid" pwd pack
-echo "# Password: ${pwd} / ${pack}"
 
 # write it to the dump
 Poke  133,0=$pwd       # password
-Poke  134,0=$pack      # pack (password-ACK)
+Poke  134,0=$pack      # pack
 
 # sanity readback
 myVar=`Peek 133,0:6`
-echo "# Verify data patch: $myVar"
+echo "# readback: $myVar"
 
 # try and use the password
-echo -n "# Use password > "
 Send "hf mfu rdpg -p 133 -k $pwd"
-Listen -c -q
-echo -n "  * Password ACK "
+Listen -c
 [[ "${CAP[*]}" == *"PACK: ${pack}"* ]] && {
-	echo "[OK]"
+	echo "password verified"
 } || {
-	echo "[FAIL]"
+	echo "password failed"
 }
 
 #==============================================================================
 # ----- print and save the result
 #
-
-dbID=`Peek 122,0:8`
-FN_BIN="amiibo-${dbID}-${tuid}-${pwd}.bin"
-
+printf "\n---\n%s\n---\n"  $tag
 xxx="`sed -E 's/(..)/\1 /g' <<<$tag`"
-xxd -p -r <<<$xxx >"${FN_BIN}"
-
-hexdump -C "${FN_BIN}"
-echo "* Saved as : ${FN_BIN}"
+printf "%s %s %s %s | "  $xxx
+xxd -p -r <<<$xxx >xtest.bin
 
 #==============================================================================
 # ----- cleanup is automatic
